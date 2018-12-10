3e83c3862b0f34a8922586f363922bc342fcb9b9
HAYET Marc-Antoine <marcantoinehayet@gmail.com>
2018-12-10 15:25:05 +0100

    [Configuration] Configuration pour le premier démarrage.

--- src/main/java/auberge/auberge/user/pi/Application.java
+++ src/main/java/auberge/auberge/user/pi/Application.java
@@ -1,12 +1,24 @@
 package auberge.auberge.user.pi;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@SpringBootApplication
-public class Application {
-
-	public static void main(String[] args) {
+@Controller
+@EnableAutoConfiguration
+public class Application extends SpringBootServletInitializer
+{
+	
+	public static void main(String[] args)
+	{
 		SpringApplication.run(Application.class, args);
+	}
+	
+	@RequestMapping(value = "/")
+	String home()
+	{
+		return "home";
 	}
 }
